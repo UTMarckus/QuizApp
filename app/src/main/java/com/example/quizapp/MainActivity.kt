580a7dbfee7dba1b6486f55cfbb33f0c5f719408
T. Ustiugov <utmarckus@gmail.com>
2025-08-05 12:52:22 +0300

    feat: add main activity logic and base questions activity

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.ui.QuestionsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.button_start)
+        val editTextName: EditText = findViewById(R.id.name)
+
+        startButton.setOnClickListener {
+            if (editTextName.text.isNotEmpty()) {
+                Intent(this@MainActivity, QuestionsActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "Please, enter your name", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.R
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
