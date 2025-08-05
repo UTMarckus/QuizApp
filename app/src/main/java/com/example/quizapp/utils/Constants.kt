47ed0b3d8318ac31a82f14c399e6f324754bff22
T. Ustiugov <utmarckus@gmail.com>
2025-08-05 15:07:09 +0300

    feat: add questions and flags

---
+++ app/src/main/java/com/example/quizapp/utils/Constants.kt
@@ -0,0 +1,123 @@
+package com.example.quizapp.utils
+
+import com.example.quizapp.R
+import com.example.quizapp.model.Question
+
+object Constants {
+
+    fun getQuestions(): MutableList<Question> {
+        val questions = mutableListOf<Question>()
+
+        questions.add(Question(
+            1,
+            "What country does this flag belong?",
+            R.drawable.flag_of_argentina,
+            "Argentina",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            2,
+            "What country does this flag belong?",
+            R.drawable.flag_of_brazil,
+            "Brazil",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            3,
+            "What country does this flag belong?",
+            R.drawable.flag_of_finland,
+            "Finland",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            4,
+            "What country does this flag belong?",
+            R.drawable.flag_of_france,
+            "France",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            5,
+            "What country does this flag belong?",
+            R.drawable.flag_of_germany,
+            "Germany",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            6,
+            "What country does this flag belong?",
+            R.drawable.flag_of_india,
+            "India",
+            "Argentina",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            7,
+            "What country does this flag belong?",
+            R.drawable.flag_of_italy,
+            "Italy",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            8,
+            "What country does this flag belong?",
+            R.drawable.flag_of_nigeria,
+            "Nigeria",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            9,
+            "What country does this flag belong?",
+            R.drawable.flag_of_romania,
+            "Romania",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        questions.add(Question(
+            10,
+            "What country does this flag belong?",
+            R.drawable.flag_of_spain,
+            "Spain",
+            "India",
+            "Iran",
+            "Ireland",
+            1
+        ))
+
+        return questions
+    }
+}
