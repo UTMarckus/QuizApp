85803b9b251702158544581495a22c5350735bda
T. Ustiugov <utmarckus@gmail.com>
2025-08-06 12:44:00 +0300

    feat: add question layout

--- app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.quizapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.R
+import com.example.quizapp.utils.Constants
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
